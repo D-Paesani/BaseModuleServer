9a2845c7f83b8d2230ba9d67f4e46ee16556eb35
mushu <coppolino@lns.infn.it>
2024-07-29 15:41:38 +0200

    aggiunta pagina delle temperature con implementazione base Thread

---
+++ bms/controller/dbmanager.py
@@ -0,0 +1,34 @@
+from bms.web_manager import db
+from datetime import datetime
+from sqlalchemy.exc import IntegrityError
+
+# class Dudev(db.Model):
+#     __bind_key__ = 'secondary'
+#     __tablename__ = 'device'
+    
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     ip = db.Column(db.String(15), nullable=False)
+#     du = db.Column(db.Integer, nullable=False)
+#     iddevicetype = db.Column(db.Integer, db.ForeignKey('devicetype.id'))
+
+# class DeviceType(db.Model):
+#     __bind_key__ = 'secondary'
+#     __tablename__ = 'devicetype'
+    
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     type = db.Column(db.String(4), unique=True, nullable=False)
+
+#     temperatures = db.relationship("Temperature", backref="devicetype", lazy=True)
+#     dudevs = db.relationship("Dudev", backref="devicetype", lazy=True)
+
+class Temperature(db.Model):
+    __bind_key__ = 'secondary'
+    __tablename__ = 'temperature'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    temperature = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    du = db.Column(db.Integer, nullable=False)
+    clb_ip = db.Column(db.String(15))
+    wwrsa_ip = db.Column(db.String(15))
+    wwrsb_ip = db.Column(db.String(15))
