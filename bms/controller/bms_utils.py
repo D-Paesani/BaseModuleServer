07b8387cce35645073e7ac5c8ac46babf7c604a5
mushu <coppolino@lns.infn.it>
2024-10-14 12:05:22 +0200

    modifica del calcolo indirizzo wet e clb a mare (+100 => jb)

--- bms/controller/bms_utils.py
+++ bms/controller/bms_utils.py
@@ -2,12 +2,16 @@ import subprocess
 
 def getwwrsips(duno):
     if duno == 0: return [f'10.0.1.20{ii+1}' for ii in range(2)]
-    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + 4*(duno>32 and duno<230) 
+    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
+     4*(duno>=32 and duno<=46) + \
+     5*(duno>=47 and duno<=60) + 100
     return [F'10.{int(jb)}.{duno}.20{ii+1}' for ii in range(2)]
 
 def getbaseip(duno, floor=100):
     if duno == 0: return '10.0.1.100'
-    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + 4*(duno>32 and duno<230) 
+    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
+     4*(duno>=32 and duno<=46) + \
+     5*(duno>=47 and duno<=60) + 100
     return F'10.{int(jb)}.{duno}.{floor}'
 
 def printdebug(deb, msg):
