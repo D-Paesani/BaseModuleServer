ce14017123c4068f9c03837a1b1c3df38b3c0d03
bm-integration <coppolino@lns.infn.it>
2025-07-07 11:51:46 +0200

    up, corretto il calcolo della posizione jb

--- bms/bms/controller/bms_utils.py
+++ bms/bms/controller/bms_utils.py
@@ -2,16 +2,18 @@ import subprocess
 
 def getwwrsips(duno):
     if duno == 0: return [f'10.0.1.20{ii+1}' for ii in range(2)]
-    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
-     4*(duno>=32 and duno<=46) + \
-     5*(duno>=47 and duno<=60) + 100
+    jb = 1*(duno<=8) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
+     4*(duno>=33 and duno<=46) + \
+     5*(duno>=47 and duno<=60)
+    jb = jb + (100 if jb >= 4 else 0)
     return [F'10.{int(jb)}.{duno}.20{ii+1}' for ii in range(2)]
 
 def getbaseip(duno, floor=100):
     if duno == 0: return '10.0.1.100'
-    jb = 1*(duno<=9) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
-     4*(duno>=32 and duno<=46) + \
-     5*(duno>=47 and duno<=60) + 100
+    jb = 1*(duno<=8) + 2*(duno>=9 and duno<=20) + 3*(duno>=21 and duno<=32) + \
+     4*(duno>=33 and duno<=46) + \
+     5*(duno>=47 and duno<=60)
+    jb = jb + (100 if jb >= 4 else 0)
     return F'10.{int(jb)}.{duno}.{floor}'
 
 def printdebug(deb, msg):
